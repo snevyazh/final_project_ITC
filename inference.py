abc836909fd57c70d5e36dc9e7ecc63ebe13474a
Stanislav Nevyazhskiy <snevyazh@gmail.com>
2023-03-15 19:07:48 +0200

    inference.py edited, added pickle model

--- inference.py
+++ inference.py
@@ -1,26 +1,30 @@
 import pandas as pd
 import numpy as np
-
-import librosa
+import pickle
 from surfboard import feature_extraction
 
 from flask import Flask
 from flask import request
 
+with open('/Users/stanislavnevyazhsky/My Drive/Colab Notebooks/Project/model.pkl', 'rb') as trained_model_dump:
+    loaded_model = pickle.load(trained_model_dump)
+
+app = Flask(__name__)
+
 @app.route('/predict_pneumonia')
 def predict_pneumonia():
     config = {'log_melspec': {'hop_length_seconds': 0.02, 'n_fft_seconds': 0.08,'n_mels': 64},
     'loudness_slidingwindow': {'frame_length_seconds': 1.0,'hop_length_seconds': 0.25}}
-    path = request.args.get('path'))
+    path = request.args.get('path')
     X_temp = (
     feature_extraction.extract_features_from_paths(
-        path,
+        [path],
         components_list=[{key: config[key]} for key in config],
         statistics_list=["mean", "std"],
     )
     .replace(-np.inf, np.nan)
     .fillna(method="bfill"))
-    y_temp = classifier_SVM.predict(X_temp)
+    y_temp = loaded_model.predict(X_temp)
     print(y_temp)
     return str(y_temp)
 

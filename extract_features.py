c67b7383dc70c379c6d90bb4ae0cea05dd843620
Leonid Astrin <astrinleonid@gmail.com>
2023-03-25 11:07:28 +0300

    Feature extraction debug

--- extract_features.py
+++ extract_features.py
@@ -1,7 +1,8 @@
 import surfboard
 import pandas as pd
 import numpy as np
-from surfboard import feature_extraction
+from surfboard import feature_extraction, sound
+from surfboard.sound import Waveform
 
 config = {
     'log_melspec': {
@@ -18,22 +19,28 @@ config = {
 path_train = pd.read_csv('path_train.csv')
 path_test = pd.read_csv('path_test.csv')
 
+for path in path_train['path']:
+    print(f"Extracing record {path}")
+    wave = Waveform(path=path, sample_rate=22050)
+    print("Success")
 
-X_train = (
-    feature_extraction.extract_features_from_paths(
-        path_train['path'],
-        components_list=[{key: config[key]} for key in config],
-        statistics_list=["mean", "std"],
-    )
-    .replace(-np.inf, np.nan)
-    .fillna(method="bfill")
-)
 
-X_test = (
-    feature_extraction.extract_features_from_paths(
-        path_test['path'],
-        components_list=[{key: config[key]} for key in config],
-        statistics_list=["mean", "std"],
-    )
-    .replace(-np.inf, np.nan)
-    .fillna(method="bfill"))
+
+# X_train = (
+#     feature_extraction.extract_features_from_paths(
+#         path_train['path'],
+#         components_list=[{key: config[key]} for key in config],
+#         statistics_list=["mean", "std"],
+#     )
+#     .replace(-np.inf, np.nan)
+#     .fillna(method="bfill")
+# )
+#
+# X_test = (
+#     feature_extraction.extract_features_from_paths(
+#         path_test['path'],
+#         components_list=[{key: config[key]} for key in config],
+#         statistics_list=["mean", "std"],
+#     )
+#     .replace(-np.inf, np.nan)
+#     .fillna(method="bfill"))
